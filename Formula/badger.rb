c95221cf04c7fb001f33a9383b51b6f1ffd6f9c9
Alexandru Geana <alex.geana@erepubliklabs.com>
2020-08-31 19:54:19 +0300

    Update badger.rb

--- Formula/badger.rb
+++ Formula/badger.rb
@@ -1,9 +1,9 @@
 class Badger < Formula
   desc "Badger is a command-line tool that adds labels to your app icon"
   homepage "https://github.com/hex/Badger"
-  url "https://github.com/hex/Badger/releases/download/v2020.8.3/Badger-osx-x64.zip"
-  version "2020.8.3"
-  sha256 "d40d67752e6424e2555008b7a6b39d289669974607305b599bf94f275c9f1af1"
+  url "https://github.com/hex/badger/releases/download/v2020.8.26/badger-osx-x64.zip"
+  version "2020.8.26"
+  sha256 "c8d9839716a0d2c89e6fc3b5a02001e096f26bcacbd28ed65915dbaa9ce38377"
   license "MIT"
 
   def install
