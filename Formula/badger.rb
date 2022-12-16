87bb06ddd80400f793738af9dfbae0e6275427e7
hex <hexaust@gmail.com>
2022-12-16 12:19:49 +0200

    Update badger.rb
    Version 2022.12.2

--- Formula/badger.rb
+++ Formula/badger.rb
@@ -1,9 +1,11 @@
 class Badger < Formula
+  arch arm: "osx-arm64", intel: "osx-x64"
+  version "2022.12.2"
   desc "Command-line tool that adds labels to your app icon"
   homepage "https://github.com/hex/Badger"
-  url "https://github.com/hex/badger/releases/download/v2020.9.2/badger-osx-x64.zip"
-  version "2020.9.2"
-  sha256 "d9d1ee20bf2b2d4b9dc27c7d065832254e5aa46853a64d0c35911149a596f5e7"
+  url "https://github.com/hex/badger/releases/download/v#{version}/badger-#{arch}.zip"
+  sha256 arm:   "0965597fb5c5bd252407afcec7daae8b4460a6676ffc691eaacc7dc7e7de1ec9",
+         intel: "af6836783cb038e300123737e0cbbf01ab7ae2f6574317d11e17dcbc24ed9ae2"
   license "MIT"
 
   def install
