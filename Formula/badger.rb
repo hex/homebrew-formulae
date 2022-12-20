4ab71e6b0f154eb3969090de79a4687d87626854
hex <hexaust@gmail.com>
2022-12-20 18:02:55 +0200

    Update badger.rb
    Add 2022.12.5 version

--- Formula/badger.rb
+++ Formula/badger.rb
@@ -6,18 +6,18 @@ class Badger < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hex/badger/releases/download/v2022.12.2/badger-osx-x64.zip"
-      sha256 "af6836783cb038e300123737e0cbbf01ab7ae2f6574317d11e17dcbc24ed9ae2"
+      url "https://github.com/hex/badger/releases/download/v2022.12.5/badger-osx-x64.zip"
+      sha256 "5cb06d39c62468376cb1bdd6ec072d19e0c80416419f8e9819599bc9fa387e19"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/hex/badger/releases/download/v2022.12.2/badger-osx-arm64.zip"
-      sha256 "0965597fb5c5bd252407afcec7daae8b4460a6676ffc691eaacc7dc7e7de1ec9"
+      url "https://github.com/hex/badger/releases/download/v2022.12.5/badger-osx-arm64.zip"
+      sha256 "088208fcd00746726d9b4396893ce1a15e81fa6ef1d46b3c4a2eb1a44af636be"
     end
-  end       
+  end
 
   on_linux do
-    url "https://github.com/hex/badger/releases/download/v2022.12.2/badger-linux-x64.zip"
-    sha256 "272cc19043f01f18047c4f88afb1f99e11b6bf00523433af3d9f3bc1f0849379"
+    url "https://github.com/hex/badger/releases/download/v2022.12.5/badger-linux-x64.zip"
+    sha256 "d2ce09700f5b204927b41f6ee4a236490f50fe4525dc4c391cf8408eadb100be"
   end
 
   def install
